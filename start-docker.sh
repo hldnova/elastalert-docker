aba013cc76d0ab021ef72a19c7df8de5851f5c05
Lida He <lida.he@dell.com>
2017-05-11 23:09:10 -0400

    remove internal ip address

--- start-docker.sh
+++ start-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-NAME=elastalert-1
+NAME=elastalert
 docker rm -f $NAME
 
 docker run -d --name $NAME \
            -h $(hostname -f) \
-           -e "ELASTICSEARCH_HOST=10.247.134.224" \
+           -e "ELASTICSEARCH_HOST=elasticsearch" \
            -e "ELASTICSEARCH_PORT=9200" \
            -e "EMAIL_ADDRESS=root@localhost" \
            -v $PWD/config/config.yaml:/opt/elastalert/elastalert_config.yaml \
            -v $PWD/rules/:/opt/elastalert/rules/ \
-    elastalert
+    emccorp/docker-elastalert:0.1.12
