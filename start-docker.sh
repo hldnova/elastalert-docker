c8a71a8b1f2a3c75899fd82bcf738d9fe93ec4c8
Lida He <lida.he@dell.com>
2017-05-12 17:33:33 -0400

    set default values for alerters

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -uxe
+set -ux
 
 NAME=elastalert
 docker rm -f $NAME
@@ -9,7 +9,7 @@ docker run -d --name $NAME \
            -e "ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-elasticsearch}" \
            -e "ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}" \
            -e "EMAIL_ADDRESS=${EMAIL_ADDRESS:-root@localhost}" \
-           -e "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL:-your-slack-webhook-url}" \
+           -e "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL:-http://localhost}" \
            -v $PWD/config/:/opt/elastalert/config/ \
            -v $PWD/rules/:/opt/elastalert/rules/ \
     emccorp/docker-elastalert:0.1.12
